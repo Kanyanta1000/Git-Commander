68a70bf3dd24cbfd16046d532d85ea5f66338b45
Kanyanta Mubanga <emailkanyanta@gmail.com>
2020-10-09 16:49:06 +0300

    quizes: [String : [Question]] dictionary with quizBank struct to enable fetching of quizzes by topic

--- Git Commander/Models/QuizBrain.swift
+++ Git Commander/Models/QuizBrain.swift
@@ -8,8 +8,10 @@
 import Foundation
 
 struct QuizBrain {
+    
     struct quizBank {
-        let gitBasics = [
+    var quizes: [String : [Question]] = [
+        "Git Basics" : [
             Question(q: "Create empty Git repo in specified directory <directory>. Run with no arguments to initialize the current directory as a git repository.", a: "init", a2: "<directory>"),
             Question(q: "Clone repo located at <repo> onto local machine. Original repo can be located on the local filesystem or on a remote machine via HTTP or SSH.", a: "clone", a2: "<repo>"),
             Question(q: "Define author name to be used for all commits in current repo. Devs commonly use --global flag to set config options for current user.", a: "config user.name", a2: "<name>"),
@@ -18,16 +20,17 @@ struct QuizBrain {
             Question(q: "List which files are staged, unstaged, and untracked.", a: "status", a2: ""),
             Question(q: "Display the entire commit history using the default format. For customization see additional options.", a: "log", a2: ""),
             Question(q: "Show unstaged changes between your index and working directory.", a: "diff", a2: "")
-        ]
+        ],
         
-        let undoingChanges = [
+        "Undoing Changes" : [
             Question(q: "Create new commit that undoes all of the changes made in <commit>, then apply it to the current branch.", a: "revert", a2: "<commit>"),
             Question(q: "Remove <file> from the staging area, but leave the working directory unchanged. This unstages a file without overwriting any changes.", a: "reset", a2: "<file>"),
             Question(q: "Remove <file> from the staging area, but leave the working directory unchanged. This unstages a file without overwriting any changes.", a: "clean", a2: "-n")
-        ]  
+        ]
+    ]
     }
-    
-    var quiz = quizBank().undoingChanges
+
+    var quiz = quizBank().quizes["Git Basics"]!
     var questionNumber = 0
     var score = 0
     
@@ -48,6 +51,8 @@ struct QuizBrain {
             score = 0
         }
     }
+    
+
     
     func getHint() -> String {
         
