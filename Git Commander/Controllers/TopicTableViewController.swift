4d2d504c598f7152532036deb14c80d1680dfa6c
Kanyanta Mubanga <emailkanyanta@gmail.com>
2020-10-10 20:04:45 +0300

    implemented tableview showing topics

--- Git Commander/Controllers/TopicTableViewController.swift
+++ Git Commander/Controllers/TopicTableViewController.swift
@@ -5,10 +5,33 @@
 //  Created by Kanyanta Mubanga on 09.10.2020.
 //
 
-import Foundation
 import UIKit
 
 
 class TopicTableViewController: UITableViewController {
+    
+    let myQuizes = quizBank().quizes
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+//    MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TopicCell", for: indexPath)
+        let quizTopics = Array(myQuizes.keys)
+        cell.textLabel?.text = quizTopics[indexPath.row]
+        
+        return cell
+    }
+    
+//    MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        myQuizes.count
+    }
+    
+//    MARK: - TableView Data Manipulation Methods
+    
     
 }
